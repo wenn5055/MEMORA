88b925cdda38357ff336f4378d95b3437c5220f3
wenn5055 <wendytangxd@gmail.com>
2026-04-07 13:17:47 +0800

    updated Eraser script isHeld visibility

--- Assets/Examples/PersistentPen/Eraser/UdonEraser.cs
+++ Assets/Examples/PersistentPen/Eraser/UdonEraser.cs
@@ -1,5 +1,6 @@
 ﻿using UdonSharp;
 using UnityEngine;
+using VRC.SDK3.Components;
 using VRC.SDK3.Data;
 using VRC.SDKBase;
 
@@ -7,32 +8,86 @@ namespace VRC.Examples.Persistence.PersistentPen
 {
     public class UdonEraser : UdonSharpBehaviour
     {
+        #region serialized fields
+        [SerializeField][Tooltip("The renderer for the ERASER mesh, to update its color")] private Renderer eraserRenderer;
+        #endregion 
+
         private DataList targetDataList;
+
+        // The Pickup component that allows the pen to be picked up and used
+        private VRCPickup pickup;
+        private MeshRenderer eraserBody;
+        private Collider eraserCollider;
+        // Whether pickup is held, changes visibility for others
+        [UdonSynced, FieldChangeCallback(nameof(IsHeld))] private bool _isHeld;
+
+        public bool IsHeld
+        {
+            set
+            {
+                _isHeld = value;
+                SetVisibleForOthers(value);
+            }
+            get => _isHeld;
+        }
 
         private void Start()
         {
+            pickup = (VRCPickup)GetComponent(typeof(VRCPickup));
+            eraserCollider = GetComponent<Collider>();
+            eraserBody = GetComponent<MeshRenderer>();
+
             targetDataList = new DataList();
+            // Disable the pen for others, hide until owner picks it up
+            if (!Networking.IsOwner(gameObject))
+            {
+                eraserCollider.enabled = false;
+                pickup.enabled = false;
+                SetVisibleForOthers(false);
+            }
+        }
+
+        private void SetVisibleForOthers(bool value)
+        {
+            if (Networking.IsOwner(gameObject))
+                return;
+
+            if (Utilities.IsValid(eraserRenderer))
+                eraserRenderer.enabled = value;
+
+            if (Utilities.IsValid(eraserBody))
+                eraserBody.enabled = value;
+        }
+        public override void OnPickup()
+        {
+            IsHeld = true;
+            RequestSerialization();
+        }
+
+        public override void OnDrop()
+        {
+            IsHeld = false;
+            RequestSerialization();
         }
 
         // On Use, Deactivate LineRenderer and Clear the Target
         public override void OnPickupUseDown()
         {
-            if (targetDataList.Count == 0) return;
-
             for (int i = targetDataList.Count - 1; i >= 0; --i)
             {
                 var targetCollider = (Collider)targetDataList[i].Reference;
-                var targetPenLine = targetCollider.GetComponent<UdonPenLine>();
-                if (Utilities.IsValid(targetPenLine))
+                if (Utilities.IsValid(targetCollider))
                 {
-                    if (Networking.IsOwner(targetPenLine.gameObject))
+                    var targetPenLine = targetCollider.GetComponent<UdonPenLine>();
+                    if (Utilities.IsValid(targetPenLine) && Networking.IsOwner(targetPenLine.gameObject))
                     {
                         targetPenLine.Erase();
                     }
 
-                    targetDataList.Remove(targetCollider);
                     InputManager.EnableObjectHighlight(targetCollider.gameObject, false);
                 }
+
+                targetDataList.RemoveAt(i);
             }
         }
 
@@ -48,8 +103,11 @@ namespace VRC.Examples.Persistence.PersistentPen
                 return;
             }
 
-            targetDataList.Add(other);
-            InputManager.EnableObjectHighlight(other.gameObject, true);
+            if (!targetDataList.Contains(other))
+            {
+                targetDataList.Add(other);
+                InputManager.EnableObjectHighlight(other.gameObject, true);
+            }
         }
 
         // On Trigger Exit, Re-Enable Target and Clear It
@@ -62,7 +120,7 @@ namespace VRC.Examples.Persistence.PersistentPen
                 if (targetCollider == other)
                 {
                     InputManager.EnableObjectHighlight(targetCollider.gameObject, false);
-                    targetDataList.Remove(targetCollider);
+                    targetDataList.RemoveAt(i);
                     break;
                 }
             }
